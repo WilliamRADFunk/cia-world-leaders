79f154f2f450b949f04dcc3da9bdcd2d781520c7
WilliamRADFunk <CONTACT@WILLIAMRADFUNK.com>
2019-05-31 21:31:08 -0400

    Uses the latest ontology from funktologies.

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 const MAIN_INSTANCE_PATH = 'http://williamrobertfunk.com/instance/';
-const COUNTRY_ONT_PATH = 'http://williamrobertfunk.com/ontologies/world-factbook#';
-const MAIN_ONT_PATH = 'http://williamrobertfunk.com/ontologies/world-leaders#';
+const COUNTRY_ONT_PATH = 'http://williamrobertfunk.com/ontologies/country#';
+const LEADERS_ONT_PATH = 'http://williamrobertfunk.com/ontologies/world-leaders#';
 const FOAF_ONT_PATH = 'http://xmlns.com/foaf/0.1/';
 
 const BASE = {
@@ -20,13 +20,13 @@ const ONTOLOGY = {
 	DT_ISO_CODE: COUNTRY_ONT_PATH + 'countryCodeISO',
 	DT_LAST_NAME: FOAF_ONT_PATH + 'lastName',
 	DT_NAME: FOAF_ONT_PATH + 'name',
-	DT_REGION_SPECIFIC: MAIN_ONT_PATH + 'regionSpecific',
-	DT_TITLE: MAIN_ONT_PATH + 'title',
+	DT_REGION_SPECIFIC: LEADERS_ONT_PATH + 'regionSpecific',
+	DT_TITLE: LEADERS_ONT_PATH + 'title',
 	// Ontology definition paths for (predicate) object/relation properties
-	HAS_APPOINTED_GOVERNMENT_OFFICE: MAIN_ONT_PATH + 'appointedGovernmentOffice',
+	HAS_APPOINTED_GOVERNMENT_OFFICE: LEADERS_ONT_PATH + 'appointedGovernmentOffice',
 	HAS_COUNTRY: COUNTRY_ONT_PATH + 'hasCountry',
-	HAS_GOVERNMENT_OFFICE: MAIN_ONT_PATH + 'hasGovernmentOffice',
-	HAS_GOVERNMENT_OFFICIAL: MAIN_ONT_PATH + 'hasGovernmentOfficial',
+	HAS_GOVERNMENT_OFFICE: LEADERS_ONT_PATH + 'hasGovernmentOffice',
+	HAS_GOVERNMENT_OFFICIAL: LEADERS_ONT_PATH + 'hasGovernmentOfficial',
 	// Instance definition paths
 	INST_COUNTRY: MAIN_INSTANCE_PATH + 'Country/',
 	INST_GOVERNMENT_OFFICE: MAIN_INSTANCE_PATH + 'GovernmentOffice/',
@@ -34,11 +34,11 @@ const ONTOLOGY = {
 	// Base path for all things instance definition
 	MAIN_INSTANCE_PATH,
 	// Base path for all things ontology definition
-	MAIN_ONT_PATH,
+	MAIN_ONT_PATH: LEADERS_ONT_PATH,
 	// Ontology class definition paths
 	ONT_COUNTRY: COUNTRY_ONT_PATH + 'Country',
-	ONT_GOVERNMENT_OFFICE: MAIN_ONT_PATH + 'GovernmentOffice',
-	ONT_PERSON: MAIN_ONT_PATH + 'Person',
+	ONT_GOVERNMENT_OFFICE: LEADERS_ONT_PATH + 'GovernmentOffice',
+	ONT_PERSON: LEADERS_ONT_PATH + 'Person',
 };
 
 const RDFS = {
